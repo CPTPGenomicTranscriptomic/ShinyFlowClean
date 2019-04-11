18c502947193670992e0c048b313d8d1f4c3e341
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2019-04-11 15:23:18 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
 #  titlePanel("FlowClean"),
 br(),
   headerPanel(
-    list(HTML('<img src="Logo_cptp.png"/>'), HTML('<a href="https://www.cptp.inserm.fr/en/technical-platforms/genomic-and-transcriptomic/">Genomic and transcriptomic plateform</a>'), HTML('<img src="Logo_inserm.png"/>')),
+    list(HTML('<img src="Logo_cptp.png"/>'), HTML('<a href="https://www.cptp.inserm.fr/en/technical-platforms/genomic-and-transcriptomic/">Genomic and transcriptomic platform</a>'), HTML('<img src="Logo_inserm.png"/>')),
     windowTitle="My Title"
   ),
 br(),
